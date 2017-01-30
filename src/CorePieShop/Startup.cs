07df00ba85bf3b41c55dce2eb2103bc1b61336e7
Sean <seon2you@hotmail.com>
2017-01-30 02:59:27 -0800

    models and viewmodel and view

--- src/CorePieShop/Startup.cs
+++ src/CorePieShop/Startup.cs
@@ -7,15 +7,28 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using CorePieShop.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace CorePieShop
 {
     public class Startup
     {
+        private IConfigurationRoot _configurationRoot;
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //services.AddDbContext<AppDbContext>(options => /options.UseSqlServer(_configurationRoot.GetConnectionString("DefaultConnection")));
+
+            services.AddTransient<ICategoryRepository, MockCategoryRepository>();
+            services.AddTransient<IPieRepository, MockPieRepository>();
+
+            //services.AddTransient<IPieRepository, PieRepository>();
+            //services.AddTransient<ICategoryRepository, CategoryRepository>();
+
             services.AddMvc();
         }
 
@@ -38,6 +51,8 @@ namespace CorePieShop
             app.UseStatusCodePages();
             app.UseStaticFiles();
             app.UseMvcWithDefaultRoute();
+
+            //DbInitializer.Seed(app);
         }
     }
 }
---
+++ src/CorePieShop/ViewModels/PiesListViewModel.cs
@@ -0,0 +1,14 @@
+﻿using CorePieShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CorePieShop.ViewModels
+{
+    public class PiesListViewModel
+    {
+        public IEnumerable<Pie> Pies { get; set; }
+        public string CurrentCategory { get; set; }
+    }
+}
