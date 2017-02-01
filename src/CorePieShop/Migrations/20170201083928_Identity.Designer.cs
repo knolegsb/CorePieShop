451f580a43f5e42ed88cabdd01d8c10b2d138a27
Sean <seon2you@hotmail.com>
2017-02-01 00:40:45 -0800

    identity database update

--- src/CorePieShop/Migrations/20170201083928_Identity.Designer.cs
+++ src/CorePieShop/Migrations/20170201083928_Identity.Designer.cs
@@ -8,9 +8,10 @@ using CorePieShop.Models;
 namespace CorePieShop.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20170201083928_Identity")]
+    partial class Identity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.0.1")
